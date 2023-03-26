050ba7183acd32e352536fc557a985a1083621be
Roy <roy@RoydeMacBook-Pro.local>
2023-03-26 22:36:25 +0800

    feat: add home layout

---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { RouterView, RouterLink, useRouter } from 'vue-router'
+defineProps<{
+  msg: string
+}>()
+const router = useRouter()
+const tip = () => alert('用户中心正在开发中，敬请期待:)')
+</script>
+
+<template>
+  <div class="top">
+    <span className="iconfont menu" @click="tip"> &#xe65c; </span>
+    <span class="title">云音悦</span>
+    <span class="iconfont search" @click="router.push('/search')">
+      &#xe62b;
+    </span>
+  </div>
+  <div class="tab">
+    <RouterLink to="/recommend" v-slot="{ isActive }">
+      <div :class="[isActive && 'selected']" class="tab-item">
+        <span> 推荐 </span>
+      </div>
+    </RouterLink>
+    <RouterLink to="/singers" v-slot="{ isActive }">
+      <div :class="[isActive && 'selected']" class="tab-item">
+        <span> 歌手 </span>
+      </div>
+    </RouterLink>
+    <RouterLink to="/rank" v-slot="{ isActive }">
+      <div class="tab-item" :class="[isActive && 'selected']">
+        <span> 排行榜 </span>
+      </div>
+    </RouterLink>
+  </div>
+  <RouterView />
+</template>
+
+<style lang="scss" scoped>
+.top {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 5px 10px;
+  background: #d44439;
+
+  & > span {
+    line-height: 40px;
+    color: #f1f1f1;
+    font-size: 20px;
+
+    &.iconfont {
+      font-size: 25px;
+    }
+  }
+}
+
+.tab {
+  height: 44px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  background: #d44439;
+
+  div {
+    flex: 1;
+    padding: 2px 0;
+    font-size: 14px;
+    color: #e4e4e4;
+
+    &.selected {
+      span {
+        padding: 3px 0;
+        font-weight: 700;
+        color: #f1f1f1;
+        border-bottom: 2px solid #f1f1f1;
+      }
+    }
+  }
+}
+
+.tab-item {
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,51 +1,58 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const Recommend = import('@/components/Recommend/index.vue')
-const Album = import('@/components/Album/index.vue')
-const Rank = import('@/components/Rank/index.vue')
-const Singers = import('@/components/Singers/index.vue')
-const Singer = import('@/components/Singer/index.vue')
-const Search = import('@/components/Search/index.vue')
+const HomeLayout = import('@/layouts/HomeLayout.vue')
+
+const Recommend = import('@/application/Recommend/index.vue')
+const Album = import('@/application/Album/index.vue')
+const Rank = import('@/application/Rank/index.vue')
+const Singers = import('@/application/Singers/index.vue')
+const Singer = import('@/application/Singer/index.vue')
+const Search = import('@/application/Search/index.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      redirect: '/recommend',
-    },
-    {
-      path: '/recommend',
-      component: Recommend,
+      component: HomeLayout,
       children: [
         {
-          path: ':id',
+          path: '',
+          redirect: '/recommend',
+        },
+        {
+          path: '/recommend',
+          component: Recommend,
+          children: [
+            {
+              path: ':id',
+              component: Album,
+            },
+          ],
+        },
+        {
+          path: '/singers',
+          component: Singers,
+          children: [
+            {
+              path: ':id',
+              component: Singer,
+            },
+          ],
+        },
+        {
+          path: '/rank',
+          component: Rank,
+        },
+        {
+          path: '/album/:id',
           component: Album,
         },
-      ],
-    },
-    {
-      path: '/singers',
-      component: Singers,
-      children: [
         {
-          path: ':id',
-          component: Singer,
+          path: '/search',
+          component: Search,
         },
       ],
-    },
-    {
-      path: '/rank',
-      component: Rank,
-    },
-    {
-      path: 'album/:id',
-      component: Album,
-    },
-    {
-      path: '/search',
-      component: Search,
     },
   ],
 })
